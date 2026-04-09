3028974cc307c7e7941c00ef66f088ca22bc658c
Jhonantan-CGS <jhonantangoncalves2017@gmail.com>
2026-04-09 13:39:51 -0300

    Corrige superficies claras e atualiza cache PWA

--- assets/js/app-core-1.js
+++ assets/js/app-core-1.js
@@ -39,7 +39,7 @@ window.onunhandledrejection = function(event) {
 const config = {
   app: {
     version: '20260406.02',
-  buildTag: '20260409r02',
+  buildTag: '20260409r03',
     versionStorageKey: 'cysyAppVersion'
   },
   base: {
