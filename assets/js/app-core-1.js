69164980d12e6dfe5a1aceb6cd9e0ade15084bf4
Jhonantan-CGS <jhonantangoncalves2017@gmail.com>
2026-04-09 14:52:17 -0300

    Corrige barra de clima no mobile

--- assets/js/app-core-1.js
+++ assets/js/app-core-1.js
@@ -39,7 +39,7 @@ window.onunhandledrejection = function(event) {
 const config = {
   app: {
     version: '20260406.02',
-    buildTag: '20260409r05',
+    buildTag: '20260409r06',
     versionStorageKey: 'cysyAppVersion'
   },
   base: {
